a0a2fbf5db01b13aedabc630becfb4bb00f69868
Ming Yang <ymviv@qq.com>
2021-12-01 18:23:32 +0800

    sync

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const searchImagesByText = async (text) => {
-  const { data } = await axios.get("https://api.kg.sota.wiki/v1/coco_images", {
+  const { data } = await axios.get("https://api.kg.sota.wiki/v1/images", {
     params: {
       text: text,
       lang: "en",
